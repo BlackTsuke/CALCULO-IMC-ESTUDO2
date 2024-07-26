6b0d5fb3404f1efc097c34f96ea514a1ec45dafb
Mikael Silva Cunha <100884743+BlackTsuke@users.noreply.github.com>
2024-07-26 16:13:55 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+import {modal} from './modal.js'
+import {alertError} from './alert-error.js'
+import {IMC, notNumber} from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => alertError.close()
+inputHeight.oninput = () => alertError.close()
+
+form.onsubmit = event => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+
+  const showAlertError = notNumber(weight) || notNumber(height)
+
+  if(showAlertError){
+    alertError.open()
+    return
+  }
+  alertError.close()
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modal.message.innerText = message
+  modal.open()
+}
+
